0fee3857fd9c2255f08f18b8f0dfa3ff107be793
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-12-08 11:45:53 +0530

    removed unused react-lenis import from layout component

--- client/src/app/(frontend)/layout.tsx
+++ client/src/app/(frontend)/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "../globals.css";
 import HeaderWrapper from "../components/HeaderWrapper";
-import { ReactLenis } from "lenis/react";
 import { SanityLive } from "@/sanity/lib/live";
 
 export default function RootLayout({
