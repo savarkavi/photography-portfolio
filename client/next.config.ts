69a6fec88644c34efd01c7cbeace11d34a92c362
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-12-12 15:18:33 +0530

    add new intro images and add new hostname in nextconfig

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [new URL("https://cdn.sanity.io/**")],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
   },
 };
 
